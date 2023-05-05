e4bf58a07322920ecbd3b2dc8b8ee53b63794eb2
Le Anh Vu <dragonslayer3105@gmail.com>
2023-05-05 16:41:38 +0700

    Create bounding box (hit box)
    - Create bounding box class (hit box) for game objects.

---
+++ Hitbox.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameUtils.h"
+
+#include <vector>
+
+class Hitbox {
+private:
+	std::vector<RECTF> _hitboxes;
+
+public:
+	RECTF GetBoundingBox(int = 0) const;
+	float GetBoxOffsetX(int = 0) const;
+	float GetBoxOffsetY(int = 0) const;
+	float GetBoxWidth(int = 0) const;
+	float GetBoxHeight(int = 0) const;
+
+	void AddHitbox(RECTF);
+};
