1fac9094ed254027495cb97a7ea559694902ce04
Le Anh Vu <dragonslayer3105@gmail.com>
2023-05-29 14:11:58 +0700

    Create Wag state class
    - Handle attacking state of mario raccoon form.

---
+++ WagState.cpp
@@ -0,0 +1,30 @@
+#include "Device.h"
+#include "PlayerState.h"
+#include "IdleState.h"
+#include "WagState.h"
+
+WagState::WagState(Player* player) : PlayerState(player) {}
+
+PlayerState* WagState::HandleStates() {
+	if (!_player->IsAttacking()) {
+		return new IdleState(_player);
+	}
+
+	return nullptr;
+}
+
+void WagState::Render() {
+	const float RAC_OFFSET = 4.0f;
+	if (_player->IsInPipe()) {
+		_player->_animatedSprite.PlaySpriteAnimation("RacFront", _player->_position);
+	}
+	else if (_player->IsAttacking()) {
+		_player->_animatedSprite.PlaySpriteAnimation("RacWag", {
+			_player->_position.x - RAC_OFFSET * _player->_normal.x,
+			_player->_position.y
+			},
+			{ _player->_normal.x, _player->_scale.y },
+			_alpha
+		);
+	}
+}
