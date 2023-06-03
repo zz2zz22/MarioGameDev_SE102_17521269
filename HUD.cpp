de13338df7ccd42b53a2730e37748bff8dcdf6d5
Le Anh Vu <dragonslayer3105@gmail.com>
2023-06-03 17:50:54 +0700

    HUD class
    - Play scene hud class.

---
+++ HUD.cpp
@@ -0,0 +1,235 @@
+#include "Game.h"
+#include "HUD.h"
+
+Texture* HUD::_hudTexture = nullptr;
+
+void HUD::_ParseLives() {
+	_lives.clear();
+	std::vector<unsigned int> digits = GameUtils::SplitDigit(_player->_lives);
+
+	for (const auto& digit : digits) {
+		_lives.emplace_back(std::to_string(digit));
+	}
+
+	if (_lives.size() < _LIVES_MAX_LENGTH) {
+		for (unsigned int i = 0, end = _LIVES_MAX_LENGTH - _lives.size(); i < end; ++i) {
+			_lives.emplace(_lives.begin(), "0");
+		}
+	}
+}
+
+void HUD::_ParseCoins() {
+	_coins.clear();
+	std::vector<unsigned int> digits = GameUtils::SplitDigit(_player->_coins);
+
+	for (const auto& digit : digits) {
+		_coins.emplace_back(std::to_string(digit));
+	}
+
+	if (_coins.size() < _COINS_MAX_LENGTH) {
+		for (unsigned int i = 0, end = _COINS_MAX_LENGTH - _coins.size(); i < end; ++i) {
+			_coins.emplace(_coins.begin(), "0");
+		}
+	}
+}
+
+void HUD::_ParseItems() {
+	std::vector<GameObject::GameObjectType> playerItems = _player->_bonusItems;
+	if (playerItems.empty()) {
+		return;
+	}
+
+	_items.clear();
+}
+
+void HUD::_ParseSpeedGauge() {
+	_speedGauge.clear();
+
+	float currentAcceleration = _player->_acceleration;
+	float maxAcceleration = _player->_ACCEL_THRESHOLD;
+	if (_player->_isHolding || _player->IsFlying()) {
+		if (currentAcceleration >= 1.5f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 1.75f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 2.0f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 2.5f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 3.0f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 3.5f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("Arrow");
+		}
+
+		if (currentAcceleration >= 4.0f / maxAcceleration || _player->IsFlying()) {
+			_speedGauge.emplace_back("PButton");
+		}
+	}
+}
+
+void HUD::_ParseScore() {
+	_score.clear();
+	std::vector<unsigned int> digits = GameUtils::SplitDigit(_player->_score);
+
+	for (const auto& digit : digits) {
+		_score.emplace_back(std::to_string(digit));
+	}
+
+	if (_score.size() < _SCORE_MAX_LENGTH) {
+		for (unsigned int i = 0, end = _SCORE_MAX_LENGTH - _score.size(); i < end; ++i) {
+			_score.emplace(_score.begin(), "0");
+		}
+	}
+}
+
+void HUD::_ParseTimeLeft(DWORD sceneTime) {
+	_timeLeft.clear();
+	std::vector<unsigned int> digits = GameUtils::SplitDigit(sceneTime);
+
+	for (const auto& digit : digits) {
+		_timeLeft.emplace_back(std::to_string(digit));
+	}
+
+	if (_timeLeft.size() < _TIME_MAX_LENGTH) {
+		for (unsigned int i = 0, end = _TIME_MAX_LENGTH - _timeLeft.size(); i < end; ++i) {
+			_timeLeft.emplace(_timeLeft.begin(), "0");
+		}
+	}
+}
+
+void HUD::_ParseSceneEnd() {
+	std::vector<GameObject::GameObjectType> playerItems = _player->_bonusItems;
+	if (playerItems.empty()) {
+		return;
+	}
+}
+
+void HUD::_ParseSprites(std::string line) {
+	_animatedSprite.ParseSprites(line, _hudTexture);
+}
+
+HUD::HUD(Player* player) {
+	_player = player;
+	_animationName = "NoItem";
+}
+
+HUD::~HUD() {}
+
+void HUD::ParseData(
+	std::string dataPath,
+	Texture*& texture,
+	std::vector<std::string> extraData)
+{
+	if (_hudTexture == nullptr) {
+		_hudTexture = texture;
+	}
+	Entity::ParseData(dataPath, texture, extraData);
+}
+
+void HUD::HandleStates() {}
+
+void HUD::HandleCollisionResult(LPCOLLISIONEVENT, D3DXVECTOR2&, D3DXVECTOR2&, D3DXVECTOR2&, D3DXVECTOR2&) {}
+
+void HUD::Update(DWORD sceneTime, std::vector<GameObject*>*) {
+	_ParseLives();
+	_ParseCoins();
+	_ParseItems();
+	_ParseSpeedGauge();
+	_ParseScore();
+	_ParseTimeLeft(sceneTime);
+	_ParseSceneEnd();
+}
+
+void HUD::Render() {
+	const float BG_OFFSET_X = 2.0f;
+	const float BG_OFFSET_Y = 42.0f;
+	_animatedSprite.PlaySpriteAnimation("BlackBG", { _position.x - BG_OFFSET_X, _position.y + BG_OFFSET_Y });
+	_animatedSprite.PlaySpriteAnimation("Panel", _position);
+
+	const float MARGIN_0 = 8.0f;
+	const float MARGIN_1 = 24.0f;
+	//Lives
+	const float LIVES_OFFSET_X = 98.0f;
+	const float LIVES_OFFSET_Y = 5.0f;
+	for (unsigned int i = 0; i < _lives.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_lives.at(i), D3DXVECTOR2(
+			_position.x - LIVES_OFFSET_X + (MARGIN_0 * i),
+			_position.y + LIVES_OFFSET_Y
+		));
+	}
+
+	//Coins
+	const float COINS_OFFSET_X = 6.0f;
+	const float COINS_OFFSET_Y = 3.0f;
+	for (unsigned int i = 0; i < _coins.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_coins.at(i), D3DXVECTOR2(
+			_position.x + COINS_OFFSET_X + (MARGIN_0 * i),
+			_position.y - COINS_OFFSET_Y
+		));
+	}
+
+	//Items
+	const float ITEMS_OFFSET_X = 42.0f;
+	for (unsigned int i = 0; i < _items.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_items.at(i), D3DXVECTOR2(
+			_position.x + ITEMS_OFFSET_X + (MARGIN_1 * i),
+			_position.y
+		));
+	}
+
+	//Speed gauge
+	const float GAUGE_OFFSET_X = 74.0f;
+	const float GAUGE_OFFSET_Y = 3.0f;
+	for (unsigned int i = 0; i < _speedGauge.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_speedGauge.at(i), D3DXVECTOR2(
+			_position.x - GAUGE_OFFSET_X + (MARGIN_0 * i),
+			_position.y - GAUGE_OFFSET_Y
+		));
+	}
+
+	//Score
+	const float SCORE_OFFSET_X = 74.0f;
+	const float SCORE_OFFSET_Y = 5.0f;
+	for (unsigned int i = 0; i < _score.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_score.at(i), D3DXVECTOR2(
+			_position.x - SCORE_OFFSET_X + (MARGIN_0 * i),
+			_position.y + SCORE_OFFSET_Y
+		));
+	}
+
+	//Time left
+	const float TIME_OFFSET_X = 2.0f;
+	const float TIME_OFFSET_Y = 5.0f;
+	for (unsigned int i = 0; i < _timeLeft.size(); ++i) {
+		_animatedSprite.PlaySpriteAnimation(_timeLeft.at(i), D3DXVECTOR2(
+			_position.x - TIME_OFFSET_X + (MARGIN_0 * i),
+			_position.y + TIME_OFFSET_Y
+		));
+	}
+
+	if (_player->_triggeredStageEnd && !_player->_hasBossItem) {
+		const float COURSE_OFFSET_X = 10.0f;
+		const float COURSE_OFFSET_Y = 135.0f;
+		_animatedSprite.PlaySpriteAnimation("CourseClear", D3DXVECTOR2(_position.x - COURSE_OFFSET_X, _position.y - COURSE_OFFSET_Y));
+		const float ANI_OFFSET_X = 52.0f;
+		const float ANI_OFFSET_Y = 126.0f;
+		_animatedSprite.PlaySpriteAnimation(_animationName, D3DXVECTOR2(_position.x + ANI_OFFSET_X, _position.y - ANI_OFFSET_Y));
+	}
+}
+
+void HUD::Release() {
+	_player = nullptr;
+	_animatedSprite.Release();
+	_hudTexture = nullptr;
+}
---
+++ HUD.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Player.h"
+
+class HUD : public Entity {
+private:
+	static Texture* _hudTexture;
+
+	const unsigned int _LIVES_MAX_LENGTH = 2;
+	const unsigned int _COINS_MAX_LENGTH = 2;
+	const unsigned int _SCORE_MAX_LENGTH = 7;
+	const unsigned int _TIME_MAX_LENGTH = 3;
+
+	std::string _animationName;
+
+	std::vector<std::string> _lives;
+	std::vector<std::string> _coins;
+	std::vector<std::string> _items;
+	std::vector<std::string> _speedGauge;
+	std::vector<std::string> _score;
+	std::vector<std::string> _timeLeft;
+
+	Player* _player;
+
+	void _ParseLives();
+	void _ParseCoins();
+	void _ParseItems();
+	void _ParseSpeedGauge();
+	void _ParseScore();
+	void _ParseTimeLeft(DWORD);
+	void _ParseSceneEnd();
+
+	void _ParseSprites(std::string) override;
+
+public:
+	HUD(Player*);
+	~HUD();
+
+	void ParseData(std::string, Texture*&, std::vector<std::string> = std::vector<std::string>()) override;
+
+	void HandleStates() override;
+	void HandleCollisionResult(LPCOLLISIONEVENT, D3DXVECTOR2&, D3DXVECTOR2&, D3DXVECTOR2&, D3DXVECTOR2&) override;
+
+	//Scene time
+	void Update(DWORD, std::vector<GameObject*>* = nullptr) override;
+	void Render() override;
+
+	void Release() override;
+};
